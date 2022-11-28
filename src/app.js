1831513e3c15d340114ce82555c51697102cd99a
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-11-28 08:12:54 -0300

    updating cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ exports.lambdaHandler = async (event, context) => {
     const response = {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Headers": "application/json",
+            "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Origin": "https://dak1pni58hzx7.cloudfront.net/",
             "Access-Control-Allow-Methods": "OPTIONS,POST"
         },
